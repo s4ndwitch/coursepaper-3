85b91ecd3219543064b9617a18dd644a853b7e72
s4ndwitch <sandwitch@postman.i2p>
2024-10-18 13:03:39 +0300

    aaaa

--- eqengine/__init__.py
+++ eqengine/__init__.py
@@ -1,17 +1,64 @@
 
 from rsa import PublicKey, verify
 
+from serialiser import Serialiser
+
 class EqEngine:
     
-    def __init__(self) -> None:
-        pass
+    _serialiser: Serialiser
     
-    def _verify(self, hash: bytes, signature: bytes, pubkey: PublicKey) -> bool:
+    def __init__(self, db_file="db.sqlite") -> None:
         
-        return hash == verify(signature, pubkey)
+        self._serialiser = Serialiser(db_file=db_file)
     
-    def handleData(self, data: dict) -> None:
-        pass
+    def _verify(self, author: str, text: str, signature: str, pubkey: str) -> bool:
+        
+        pub_key: PublicKey = PublicKey.load_pkcs1(pubkey)
+        
+        return verify(
+            message=(author + text),
+            signature=signature,
+            pub_key=pub_key
+        )
     
-    def handleRequest(self, data: dict) -> None:
+    def handleData(self, data: list) -> None:
+        
+        for element in data:
+            if element["type"] == "user":
+                
+                if (set([
+                    "uid", "nickname", "pubkey", "type"
+                ]) != set(element.keys())):
+                    continue
+                
+                if self._serialiser.getUser(element["uid"]) == None:
+                    
+                    self._serialiser.createUser(
+                        nickname=element["nickname"],
+                        pubkey=element["pubkey"]
+                    )
+            
+            elif element["type"] == "post":
+                
+                if (set([
+                    "uid", "author", "text", "signature", "pubkey"
+                ]) != set(element.keys())):
+                    continue
+                
+                if self._serialiser.getPost(element["uid"]) == None:
+                    if self._verify(
+                        author=element["author"],
+                        text=element["text"],
+                        signature=element["signature"],
+                        pubkey=element["pubkey"]
+                    ):
+
+                        self._serialiser.createPost(
+                            author=element["author"],
+                            text=element["text"],
+                            signature=element["signature"],
+                            pubkey=element["pubkey"]
+                        )
+    
+    def request(self, data: list) -> list:
         pass
